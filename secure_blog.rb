6bd8b756b4d18c4bf5720947a6a7017aaf548eea
S Marshall <smarshall@ieee.org>
2012-08-03 20:43:42 -0400

    changed flush path

--- secure_blog.rb
+++ secure_blog.rb
@@ -290,9 +290,9 @@ get %r{/blog/(?<permalink>[\d]+)(?<format>[\.json]*)} do
   end
 end
 
-get '/flush' do
+get '/blog/flush' do
   $CACHE.flush_all
   $post_start_time = current_time
-  $start_time = current_time
+  $start_time = $post_start_time
   redirect '/blog'
 end
